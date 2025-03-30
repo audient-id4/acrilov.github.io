cfd927e9e7db89b5e26fc6c4370f9330f0f4fa26
KrOySi <67242784+KrOySi@users.noreply.github.com>
2025-03-30 20:16:45 +0300

    START

---
+++ js/config.js
@@ -0,0 +1,45 @@
+let config = {
+
+  // Задержка загрузки (1 секунда)
+  preloaderDelay: 1,
+
+  // Задержка появления видео (4.2 секунды)
+  showDelay: 1.5, 
+
+  // Заголовок
+  title: 'acrylic',
+
+  // Социальные сети
+  social: {
+    telegram_kroysi: '@mate_beats',
+    soundcloud_kroysi: 'mate1mate',
+    instagram_kroysi: 'prod_mate_'
+  }, 
+
+
+  // Цвет иконки загрузки
+  preloaderColor: '#C21807',
+
+
+  // Цвет кнопки play
+  playButtonColor: '#C21807',
+  // Цвет тени кнопки play
+  playButtonShadowColor: 'rgba(255, 255, 255, 0.5)',
+
+
+  // Цвет заголовка
+  titleColor: '#C21807',
+  // Цвет тени загаловка
+  titleShadowColor: 'rgba(255, 255, 255, 0.5)',
+
+
+  // Текст копирайта
+  copyrightText: 'ddos n get rekt...',
+  // Цвет копирайта
+  copyrightTextColor: '#C21807',
+
+  // Цвет иконок социальных сетей
+  socialColor: '#C21807',
+  // Цвет тени иконок социальных сетей
+  socialShadowColor: 'rgba(255, 255, 255, 0.5)',
+}
