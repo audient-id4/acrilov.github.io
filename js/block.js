d2da4157aeb058dfa14659579a9c6d83df98cea0
KrOySi <67242784+KrOySi@users.noreply.github.com>
2025-04-11 08:03:56 +0300

    upd

--- js/block.js
+++ js/block.js
@@ -1,142 +1,55 @@
-(() => {
-    const _0x4c38 = ['/videos/Righthere.mp4', 'пойди нахуй', '<html><head><title>blocked</title></head><body><h1>', '</h1></body></html>', 'html|htm|php|asp|aspx|js|css|json|xml', 'debugger', 'outerWidth', 'innerWidth', 'outerHeight', 'innerHeight', 'performance', 'now', 'log', 'warn', 'error', 'info', 'debug', 'table', 'dir', 'time', 'timeEnd', 'fetch', 'XMLHttpRequest', 'open', 'prototype', 'click', 'keydown', 'contextmenu', 'copy', 'A', 'download', 'href', 'toLowerCase', 'endsWith', 'preventDefault', 'body', 'appendChild', 'removeChild', 'createElement', 'Blob', 'URL', 'createObjectURL', 'nodeName', 'SCRIPT', 'src', 'MutationObserver', 'observe', 'documentElement', 'childList', 'subtree', 'readyState', 'complete', 'load', 'addEventListener', 'apply', 'userAgent', 'test', 'keys', 'some', 'includes'];
-    const _0x12e9 = function(_0x4c380x1, _0x4c380x2) {
-        _0x4c380x1 = _0x4c380x1 - 0x0;
-        let _0x12e9x1 = _0x4c38[_0x4c380x1];
-        return _0x12e9x1;
-    };
-    const _0x2a3d = function() {
-        const _0x2a3dx1 = {
-            'REDIRECT_URL': _0x12e9('0x0'),
-            'BLOCK_MESSAGE': _0x12e9('0x1'),
-            'FAKE_HTML': _0x12e9('0x2') + _0x12e9('0x1') + _0x12e9('0x3'),
-            'PROTECTED_EXTENSIONS': new RegExp(_0x12e9('0x4'), 'i'),
-            'CHECK_INTERVAL': 0x19
+(function() {
+	let protectionTriggered = false;
+    function loadProtectedCode() {
+        const protectedLogic = function() {
+            console.log("Защищенный код выполнен");
+            
+            document.getElementById('secure-content').innerHTML = 
+                "Контент, защищенный от анализа";
         };
-        function _0x2a3dx2() {
-            try {
-                if (window[_0x12e9('0x6')] - window[_0x12e9('0x7')] > 0xa0 || window[_0x12e9('0x8')] - window[_0x12e9('0x9')] > 0xa0) return !![];
-                const _0x2a3dx3 = window[_0x12e9('0xa')][_0x12e9('0xb')]();
-                eval(_0x12e9('0x5'));
-                return window[_0x12e9('0xa')][_0x12e9('0xb')]() - _0x2a3dx3 > 0x32;
-            } catch (_0x2a3dx4) {
-                return !![];
-            }
-        }
-        function _0x2a3dx5() {
-            const _0x2a3dx6 = ['__WEBPACK_', '__REACT_', '__VUE_', '__REDUX_', 'DevTools', 'DeveloperTools', 'ChromeDev', 'Firebug', 'WebsiteDownloader', 'SavePage', 'SingleFile', 'HTTrack', 'wget', 'curl', 'scrap', 'crawl', 'grabber', 'downloader'];
-            return _0x2a3dx6[_0x12e9('0x2d')](_0x2a3dx7 => Object[_0x12e9('0x2e')](window)[_0x12e9('0x2d')](_0x2a3dx8 => _0x2a3dx8[_0x12e9('0x2f')](_0x2a3dx7)));
-        }
-        function _0x2a3dx9() {
-            return new RegExp('(chrome|firefox)[\\s\\/]*devtools|wget|curl|httrack|scrap|download', 'i')[_0x12e9('0x2c')](navigator[_0x12e9('0x2b')]);
-        }
-        function _0x2a3dxa(_0x2a3dxb) {
-            let _0x2a3dxc = _0x2a3dxb['target'];
-            while (_0x2a3dxc) {
-                if (_0x2a3dxc[_0x12e9('0x1e')] === _0x12e9('0x1f') && (_0x2a3dxc['hasAttribute'](_0x12e9('0x20')) || _0x2a3dx2['PROTECTED_EXTENSIONS'][_0x12e9('0x2c')](_0x2a3dxc[_0x12e9('0x21')][_0x12e9('0x22')]()))) {
-                    _0x2a3dxb[_0x12e9('0x23')]();
-                    const _0x2a3dxd = _0x2a3dx2['PROTECTED_EXTENSIONS'][_0x12e9('0x2c')](_0x2a3dxc[_0x12e9('0x21')]) ? _0x2a3dx2['FAKE_HTML'] : _0x2a3dx2['BLOCK_MESSAGE'];
-                    const _0x2a3dxe = _0x2a3dx2['PROTECTED_EXTENSIONS'][_0x12e9('0x2c')](_0x2a3dxc[_0x12e9('0x21')]) ? 'text/html' : 'text/plain';
-                    const _0x2a3dxf = _0x2a3dx2['PROTECTED_EXTENSIONS'][_0x12e9('0x2c')](_0x2a3dxc[_0x12e9('0x21')]) ? 'blocked.html' : 'blocked.txt';
-                    const _0x2a3dx10 = new Blob([_0x2a3dxd], {
-                        'type': _0x2a3dxe
-                    });
-                    const _0x2a3dx11 = URL[_0x12e9('0x28')](_0x2a3dx10);
-                    const _0x2a3dx12 = document[_0x12e9('0x27')](_0x12e9('0x1f'));
-                    _0x2a3dx12[_0x12e9('0x21')] = _0x2a3dx11;
-                    _0x2a3dx12[_0x12e9('0x20')] = _0x2a3dxf;
-                    document[_0x12e9('0x25')][_0x12e9('0x24')](_0x2a3dx12);
-                    _0x2a3dx12[_0x12e9('0x26')]();
-                    document[_0x12e9('0x25')][_0x12e9('0x29')](_0x2a3dx12);
-                    break;
-                }
-                _0x2a3dxc = _0x2a3dxc['parentElement'];
-            }
-        }
-        function _0x2a3dx13(_0x2a3dxb) {
-            const _0x2a3dx14 = {
-                'F12': !![],
-                'Ctrl+Shift+I': !![],
-                'Ctrl+Shift+J': !![],
-                'Ctrl+Shift+C': !![],
-                'Ctrl+U': !![],
-                'Shift+F10': !![]
-            };
-            const _0x2a3dx15 = `${_0x2a3dxb['ctrlKey']?'Ctrl+':''}${_0x2a3dxb['shiftKey']?'Shift+':''}${_0x2a3dxb['key']}`;
-            if (_0x2a3dx14[_0x2a3dx15] || _0x2a3dx14[_0x2a3dxb['key']]) {
-                _0x2a3dxb[_0x12e9('0x23')]();
-                window['location'][_0x12e9('0x21')] = _0x2a3dx2['REDIRECT_URL'];
-            }
-        }
-        function _0x2a3dx16() {
-            const _0x2a3dx17 = [_0x12e9('0xc'), _0x12e9('0xd'), _0x12e9('0xe'), _0x12e9('0xf'), _0x12e9('0x10'), _0x12e9('0x11'), _0x12e9('0x12'), _0x12e9('0x13'), _0x12e9('0x14')];
-            _0x2a3dx17[_0x12e9('0x2a')](_0x2a3dx18 => {
-                const _0x2a3dx19 = console[_0x2a3dx18];
-                console[_0x2a3dx18] = function() {
-                    if (_0x2a3dx2() || _0x2a3dx5()) {
-                        window['location'][_0x12e9('0x21')] = _0x2a3dx2['REDIRECT_URL'];
-                    }
-                    _0x2a3dx19[_0x12e9('0x30')](console, arguments);
-                };
-            });
-        }
-        function _0x2a3dx1a() {
-            const _0x2a3dx1b = window[_0x12e9('0x15')];
-            window[_0x12e9('0x15')] = function() {
-                if (_0x2a3dx2() || _0x2a3dx5()) {
-                    window['location'][_0x12e9('0x21')] = _0x2a3dx2['REDIRECT_URL'];
-                    return Promise['reject']();
-                }
-                return _0x2a3dx1b[_0x12e9('0x30')](this, arguments);
-            };
-            const _0x2a3dx1c = window[_0x12e9('0x16')][_0x12e9('0x17')];
-            window[_0x12e9('0x16')][_0x12e9('0x17')] = function() {
-                if (_0x2a3dx2() || _0x2a3dx5()) {
-                    window['location'][_0x12e9('0x21')] = _0x2a3dx2['REDIRECT_URL'];
-                    return;
-                }
-                _0x2a3dx1c[_0x12e9('0x30')](this, arguments);
-            };
-        }
-        function _0x2a3dx1d() {
-            const _0x2a3dx1e = new MutationObserver(_0x2a3dx1f => {
-                _0x2a3dx1f[_0x12e9('0x2a')](_0x2a3dx20 => {
-                    _0x2a3dx20['addedNodes'][_0x12e9('0x2a')](_0x2a3dx21 => {
-                        if (_0x2a3dx21[_0x12e9('0x1e')] === _0x12e9('0x1a') && _0x2a3dx21[_0x12e9('0x1b')] && /chrome-extension|devtools|scrap/i[_0x12e9('0x2c')](_0x2a3dx21[_0x12e9('0x1b')])) {
-                            window['location'][_0x12e9('0x21')] = _0x2a3dx2['REDIRECT_URL'];
-                        }
-                    });
-                });
-            });
-            _0x2a3dx1e[_0x12e9('0x1c')](document[_0x12e9('0x1d')], {
-                'childList': !![],
-                'subtree': !![]
-            });
-        }
-        function _0x2a3dx22() {
-            document[_0x12e9('0x18')](_0x12e9('0x19'), _0x2a3dx13);
-            document[_0x12e9('0x18')](_0x12e9('0x26'), _0x2a3dxa, !![]);
-            document[_0x12e9('0x18')](_0x12e9('0x1e'), _0x2a3dxb => _0x2a3dxb[_0x12e9('0x23')]());
-            document[_0x12e9('0x18')](_0x12e9('0x1f'), _0x2a3dxb => {
-                if (_0x2a3dx2()) _0x2a3dxb[_0x12e9('0x23')]();
-            });
-            _0x2a3dx16();
-            _0x2a3dx1a();
-            _0x2a3dx1d();
-            setInterval(() => {
-                if (_0x2a3dx2() || _0x2a3dx5() || _0x2a3dx9()) {
-                    window['location'][_0x12e9('0x21')] = _0x2a3dx2['REDIRECT_URL'];
-                }
-            }, _0x2a3dx2['CHECK_INTERVAL']);
-            if (_0x2a3dx2() || _0x2a3dx5() || _0x2a3dx9()) {
-                window['location'][_0x12e9('0x21')] = _0x2a3dx2['REDIRECT_URL'];
-            }
-        }
-        if (document[_0x12e9('0x1e')] === _0x12e9('0x1f')) {
-            _0x2a3dx22();
-        } else {
-            window[_0x12e9('0x18')](_0x12e9('0x1e'), _0x2a3dx22);
+        
+        setTimeout(protectedLogic.toString() + ';protectedLogic();', 0);
+    }
+
+    window.addEventListener('load', function() {
+        setTimeout(loadProtectedCode, 1000);
+        
+        document.addEventListener('contextmenu', function(e) {
+            e.preventDefault();
+        });
+    });
+
+    const secureElements = {
+        getContentDiv: function() {
+            return document.getElementById('secure-content');
         }
     };
-    _0x2a3d();
+
+	let lastWidth = window.innerWidth;
+    let lastHeight = window.innerHeight;
+    
+    const resizeObserver = new ResizeObserver(() => {
+        const widthDiff = Math.abs(window.innerWidth - lastWidth);
+        const heightDiff = Math.abs(window.innerHeight - lastHeight);
+        
+        if (widthDiff > 10 || heightDiff > 10) {
+            triggerProtection();
+        }
+        
+        lastWidth = window.innerWidth;
+        lastHeight = window.innerHeight;
+    });
+
+	function triggerProtection() {
+        if (!protectionTriggered) {
+            protectionTriggered = true;
+            
+            window.location.href = "/videos/Righthere.mp4";
+        }
+    }
+    
+    resizeObserver.observe(document.body);
+    
+    Object.freeze(secureElements);
+    Object.freeze(window.__proto__);
 })();
