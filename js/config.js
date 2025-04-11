a6c05a65efbcbc9f311c1b6d499b59b9f8f66332
KrOySi <67242784+KrOySi@users.noreply.github.com>
2025-04-11 08:25:14 +0300

    upd

--- js/config.js
+++ js/config.js
@@ -20,7 +20,24 @@ let config = {
   titleColor: '#9789ff',
   titleShadowColor: 'rgba(255, 255, 255, 0.5)',
 
-  copyrightText: 'luv my pretty g...',
+  copyrightText: [
+    'get inspired',
+    'tryna drop a beat? we\'ll see',
+    'beatmaking boss',
+    'stay low, acrylic hears',
+    'ur beat is wack, acrylic said so',
+    'born to make bangers',
+    'daw crashed, u\'re done',
+    '100% groove, 0% filler',
+    'try harder, sound fatter',
+    'produced by acrylic, played by all',
+    'sample me? have fun',
+    '404: rhythm not found',
+    'acrylic > you',
+    'this ain\'t garageband, it slaps',
+    'remixed, outdone, fire',
+    'get that 808 pumping'
+  ],
   copyrightTextColor: '#9789ff',
 
   socialColor: '#9789ff',
